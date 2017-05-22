8f69f45fbc646f7db337bf242215d41363693c12
suengguan <suengguan@126.com>
2017-05-22 16:22:02 +0800

    modify test file

--- tests/algorithm_test.go
+++ tests/algorithm_test.go
@@ -1,15 +1,12 @@
 package test
 
 import (
-	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"testing"
 
-	daoApi "api/dao_service"
 	_ "app-service/algorithm-service/routers"
-	//"github.com/astaxie/beego"
 	"model"
 )
 
@@ -17,56 +14,13 @@ const (
 	base_url = "http://localhost:8080/v1/algorithm"
 )
 
-func Test_Create(t *testing.T) {
-	// create admin
-	daoApi.UserDaoApi.Init("http://user-dao-service:8080")
-	daoApi.ResourceDaoApi.Init("http://resource-dao-service:8080")
-	var admin model.User
-	var resource model.Resource
-	resource.Id = 0
-	admin.Id = 0
-	admin.Name = "admin"
-	admin.Resource = &resource
-	newAdmin, err := daoApi.UserDaoApi.Create(&admin)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-	t.Log("admin:", *newAdmin)
-	resource.User = newAdmin
-	newResource, err := daoApi.ResourceDaoApi.Create(&resource)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-	t.Log("resource:", *newResource)
-
-	// create image
-	var algorithm model.Algorithm
-	algorithm.Id = 0
-	algorithm.Name = "algorithm"
-	algorithm.Version = "v1"
-	algorithm.Description = "this is algorithm test"
-	algorithm.Author = "qsg"
-	algorithm.Parameters = "param1=1"
-	algorithm.Image = ""
-	algorithm.Downloads = 0
-	algorithm.Stars = 0
-
-	// post create action
-	requestData, err := json.Marshal(&algorithm)
-	if err != nil {
-		t.Log("erro : ", err)
-		return
-	}
-
-	res, err := http.Post(base_url+"/", "application/x-www-form-urlencoded", bytes.NewBuffer(requestData))
+func Test_GetAll(t *testing.T) {
+	res, err := http.Get(base_url + "/")
 	if err != nil {
 		t.Log("erro : ", err)
 		return
 	}
 	defer res.Body.Close()
-
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Log("erro : ", err)
@@ -89,3 +43,76 @@ func Test_Create(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+// func Test_Create(t *testing.T) {
+// 	// create admin
+// 	daoApi.UserDaoApi.Init("http://user-dao-service:8080")
+// 	daoApi.ResourceDaoApi.Init("http://resource-dao-service:8080")
+// 	var admin model.User
+// 	var resource model.Resource
+// 	resource.Id = 0
+// 	admin.Id = 0
+// 	admin.Name = "admin"
+// 	admin.Resource = &resource
+// 	newAdmin, err := daoApi.UserDaoApi.Create(&admin)
+// 	if err != nil {
+// 		t.Log(err)
+// 		return
+// 	}
+// 	t.Log("admin:", *newAdmin)
+// 	resource.User = newAdmin
+// 	newResource, err := daoApi.ResourceDaoApi.Create(&resource)
+// 	if err != nil {
+// 		t.Log(err)
+// 		return
+// 	}
+// 	t.Log("resource:", *newResource)
+
+// 	// create image
+// 	var algorithm model.Algorithm
+// 	algorithm.Id = 0
+// 	algorithm.Name = "algorithm"
+// 	algorithm.Version = "v1"
+// 	algorithm.Description = "this is algorithm test"
+// 	algorithm.Author = "qsg"
+// 	algorithm.Parameters = "param1=1"
+// 	algorithm.Image = ""
+// 	algorithm.Downloads = 0
+// 	algorithm.Stars = 0
+
+// 	// post create action
+// 	requestData, err := json.Marshal(&algorithm)
+// 	if err != nil {
+// 		t.Log("erro : ", err)
+// 		return
+// 	}
+
+// 	res, err := http.Post(base_url+"/", "application/x-www-form-urlencoded", bytes.NewBuffer(requestData))
+// 	if err != nil {
+// 		t.Log("erro : ", err)
+// 		return
+// 	}
+// 	defer res.Body.Close()
+
+// 	resBody, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Log("erro : ", err)
+// 		return
+// 	}
+
+// 	t.Log(string(resBody))
+
+// 	var response model.Response
+// 	json.Unmarshal(resBody, &response)
+// 	if err != nil {
+// 		t.Log("erro : ", err)
+// 		return
+// 	}
+
+// 	if response.Reason == "success" {
+// 		t.Log("PASS OK")
+// 	} else {
+// 		t.Log("ERROR:", response.Reason)
+// 		t.FailNow()
+// 	}
+// }
